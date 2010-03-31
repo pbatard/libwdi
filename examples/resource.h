23a1f1187798bf1f2a53a3c03ee2f44a9288621d
Pete Batard <pbatard@gmail.com>
2010-03-31 01:03:49 +0100

    minor fixes (renaming, removed unwanted params, etc)

--- examples/resource.h
+++ examples/resource.h
@@ -2,12 +2,13 @@
 // Microsoft Visual C++ generated include file.
 // Used by setdrv_gui.rc
 //
-#define IDC_STATIC                      -1
 #define IDC_COMBO1                      1001
 #define IDC_ATTRIBUTES                  1004
+#define IDC_INFO                        1004
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
