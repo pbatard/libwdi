412cb97a13d5a6a626f29323f9161471ea75dd7d
Pete Batard <pbatard@gmail.com>
2010-04-16 13:35:54 +0100

    added app icon for setdrv_gui.exe

--- examples/resource.h
+++ examples/resource.h
@@ -2,6 +2,7 @@
 // Microsoft Visual C++ generated include file.
 // Used by setdrv_gui.rc
 //
+#define IDI_ICON1                       102
 #define IDC_COMBO1                      1001
 #define IDC_DEVICELIST                  1001
 #define IDC_VID                         1002
@@ -18,6 +19,7 @@
 #define IDC_INSTALL                     1009
 #define IDC_CREATE                      1010
 #define IDC_BUTTON2                     1019
+#define IDC_BROWSE                      1019
 #define IDC_COMBOBOXEX1                 1021
 #define IDC_EDIT1                       1022
 #define IDC_FOLDER                      1148
@@ -27,7 +29,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1027
 #define _APS_NEXT_SYMED_VALUE           101
