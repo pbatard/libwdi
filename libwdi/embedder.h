06c51987cf1d9e31cc8017dcda6caf40d8a5fbb6
Pete Batard <pbatard@gmail.com>
2010-07-19 00:04:19 +0100

    added SOLUTIONDIR macro definition in embedder.h
    suggested by Travis Robinson

--- libwdi/embedder.h
+++ libwdi/embedder.h
@@ -31,7 +31,6 @@
 #if defined(_MSC_VER)
 #define __STR2__(x) #x
 #define __STR1__(x) __STR2__(x)
-// TODO: feed preprocessor arg to custom step?
 #if defined(_WIN64) && defined(OPT_M32)
 // a 64 bit application/library CANNOT be used on 32 bit platforms
 #pragma message(__FILE__ "(" __STR1__(__LINE__) ") : warning : library is compiled as 64 bit - disabling 32 bit support")
@@ -43,18 +42,22 @@
  * Defines where we should we pick the 32 and 64 bit installer exes to embed
  */
 #if defined(_MSC_VER) && !defined(DDKBUILD)
+#if !defined(SOLUTIONDIR)
+#define SOLUTIONDIR ".."
+#endif
 #if defined(_DEBUG)
-#define INSTALLER_PATH_32 "..\\Win32\\Debug\\helper"
-#define INSTALLER_PATH_64 "..\\x64\\Debug\\helper"
+#define INSTALLER_PATH_32 SOLUTIONDIR "\\Win32\\Debug\\helper"
+#define INSTALLER_PATH_64 SOLUTIONDIR "\\x64\\Debug\\helper"
 #else
-#define INSTALLER_PATH_32 "..\\Win32\\Release\\helper"
-#define INSTALLER_PATH_64 "..\\x64\\Release\\helper"
+#define INSTALLER_PATH_32 SOLUTIONDIR "\\Win32\\Release\\helper"
+#define INSTALLER_PATH_64 SOLUTIONDIR "\\x64\\Release\\helper"
 #endif
 #else
+#if !defined(SOLUTIONDIR)
+#define SOLUTIONDIR "."
+#endif
 // If you compile with shared libs, DON'T PICK THE EXE IN "installer",
 // as it won't run from ANYWHERE ELSE! Use the one from .libs instead.
-#define INSTALLER_PATH_32 "."
-#define INSTALLER_PATH_64 "."
+#define INSTALLER_PATH_32 SOLUTIONDIR
+#define INSTALLER_PATH_64 SOLUTIONDIR
 #endif
-
-#define drv_sprintf(s, ...)	_snprintf(s, sizeof(s), __VA_ARGS__)
