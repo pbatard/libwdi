acb5e319c9b3aa7deb60b818135c3f32caa1382f
Pete Batard <pbatard@gmail.com>
2010-08-17 20:55:41 +0100

    fixed DDK warning C4130: logical operation on address of string constant

--- libwdi/installer.h
+++ libwdi/installer.h
@@ -50,7 +50,7 @@
 #define safe_strncmp(str1, str2, count) strncmp(((str1==NULL)?"<NULL>":str1), ((str2==NULL)?"<NULL>":str2), count)
 #define safe_closehandle(h) do {if (h != INVALID_HANDLE_VALUE) {CloseHandle(h); h = INVALID_HANDLE_VALUE;}} while(0)
 #define safe_sprintf _snprintf
-#define safe_strlen(str) ((str==NULL)?0:strlen(str))
+#define safe_strlen(str) ((str)?strlen(str):0)
 #define static_sprintf(dest, format, ...) safe_sprintf(dest, sizeof(dest), format, __VA_ARGS__)
 #define safe_swprintf _snwprintf
 #define safe_strdup _strdup
