42ca64863fc163f39e705b9a9f4f40bee55161fa
Pete Batard <pbatard@gmail.com>
2010-08-17 19:55:44 +0100

    added libusb-win32's inf-wizard to examples (MSVC)

---
+++ examples/libusb_version.h
@@ -0,0 +1,18 @@
+/* libusb_version.h ++ auto-generated
+*/
+#ifndef __LIBUSB_VERSION_H
+#define __LIBUSB_VERSION_H
+
+#define __DEFTOSTR(x) #x
+#define  _DEFTOSTR(x) __DEFTOSTR(x)
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 2
+#define VERSION_MICRO 1
+#define VERSION_NANO  0
+#define VERSION_DATE 07/28/2010
+
+#define VERSION VERSION_MAJOR.VERSION_MINOR.VERSION_MICRO.VERSION_NANO
+#define RC_VERSION VERSION_MAJOR,VERSION_MINOR,VERSION_MICRO,VERSION_NANO
+
+#endif
