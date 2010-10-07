02981941dc9f71eaa608ccbcaf10855612b711e2
Pete Batard <pbatard@gmail.com>
2010-10-07 01:02:10 +0100

    added MinGW/MS DLL interchangeability

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,25 @@
 #!/bin/sh
 
-# use glibtoolize if it is available (darwin)
+# rebuilds the Windows def file by exporting all LIBDWI API calls
+create_def()
+{
+  echo "rebuidling libwdi.def file"
+  echo "LIBRARY" > libwdi/libwdi.def
+  echo "EXPORTS" >> libwdi/libwdi.def
+  sed -n -e "s/.*LIBWDI_API.*\([[:blank:]]\)\(wdi.*\)(.*/  \2/p" libwdi/libwdi.c libwdi/vid_data.c libwdi/logging.c >> libwdi/libwdi.def
+  # We need to manually define a whole set of DLL aliases if we want the MS
+  # DLLs to be usable with dynamically linked MinGW executables. This is
+  # because it is not possible to avoid the @ decoration from import WINAPI
+  # calls in MinGW generated objects, and .def based MS generated DLLs don't
+  # have such a decoration => linking to MS DLL will fail without aliases.
+  # Currently, the maximum size is 16 and all sizes are multiples of 4
+  for i in 4 8 12 16
+  do
+    sed -n -e "s/.*LIBWDI_API.*\([[:blank:]]\)\(wdi.*\)(.*/  \2@$i = \2/p" libwdi/libwdi.c libwdi/vid_data.c libwdi/logging.c >> libwdi/libwdi.def
+  done
+}
+
+# use glibtoolize if it is available
 (glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
 
 $LIBTOOLIZE --copy --force || exit 1
@@ -15,3 +34,5 @@ autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
 ./configure --enable-toggable-debug --enable-examples-build --disable-debug --with-ddkdir="E:/WinDDK/7600.16385.0" --with-libusb0="D:/libusb-win32" $*
+# rebuild .def, if sed is available
+type -P sed &>/dev/null && create_def
