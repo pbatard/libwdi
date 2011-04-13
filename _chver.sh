9de44b2f8ae3edf201abfd51340ce708c5081d4b
Pete Batard <pbatard@gmail.com>
2011-04-13 11:32:02 +0100

    updated _chver.sh

--- _chver.sh
+++ _chver.sh
@@ -26,13 +26,15 @@ case $MICRO in *[!0-9]*)
   exit 1
 esac
 echo "changing version to $MAJOR.$MINOR.$MICRO"
-sed -e "s/^AC_INIT(.*/AC_INIT([libwdi], [$MAJOR.$MINOR.$MICRO], [libusb-devel@lists.sourceforge.net], [libwdi], [http:\/\/libusb.org\/wiki\/windows_backend])/" configure.ac > configure.ac~
+sed -e "s/^AC_INIT(\[\([^ ]*\)\], \[\([^ ]*\)\]\(.*\)/AC_INIT([\1], [$MAJOR.$MINOR.$MICRO]\3/" configure.ac > configure.ac~
 mv configure.ac~ configure.ac
 cat > cmd.sed <<\_EOF
 s/^[ \t]*FILEVERSION[ \t]*.*,.*,.*,\(.*\)/ FILEVERSION @@MAJOR@@,@@MINOR@@,@@MICRO@@,\1/
 s/^[ \t]*PRODUCTVERSION[ \t]*.*,.*,.*,\(.*\)/ PRODUCTVERSION @@MAJOR@@,@@MINOR@@,@@MICRO@@,\1/
 s/^\([ \t]*\)VALUE[ \t]*"FileVersion",[ \t]*".*,[ \t]*.*,[ \t]*.*,\(.*\)"/\1VALUE "FileVersion", "@@MAJOR@@, @@MINOR@@, @@MICRO@@,\2"/
 s/^\([ \t]*\)VALUE[ \t]*"ProductVersion",[ \t]*".*,[ \t]*.*,[ \t]*.*,\(.*\)"/\1VALUE "ProductVersion", "@@MAJOR@@, @@MINOR@@, @@MICRO@@,\2"/
+s/^\(.*\)"Zadig, Version \(.*\)\.\(.*\)"\(.*\)/\1"Zadig, Version @@MAJOR@@.@@MINOR@@.@@MICRO@@.\3"\4/
+s/^\(.*\)zadig\/zadig_v\(.*\)\.\(.*\)\.7z/\1zadig\/zadig_v@@MAJOR@@.@@MINOR@@.@@MICRO@@.\3.7z/
 _EOF
 
 # First run sed to substitute our variable in the sed command file
@@ -47,4 +49,6 @@ sed -f cmd.sed examples/zadig.rc > examples/zadig.rc~
 mv examples/zadig.rc~ examples/zadig.rc
 sed -f cmd.sed examples/wdi-simple.rc > examples/wdi-simple.rc~
 mv examples/wdi-simple.rc~ examples/wdi-simple.rc
+sed -f cmd.sed _bm.sh > _bm.sh~
+mv _bm.sh~ _bm.sh
 rm cmd.sed
