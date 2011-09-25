c3c94314be5140610995b1c2997ad6007095d25e
Pete Batard <pete@akeo.ie>
2011-09-26 00:03:49 +0100

    [internal] bumped internal version

--- _bm.sh
+++ _bm.sh
@@ -2,7 +2,7 @@
 # Create and upload a Zadig release
 # !!!THIS SCRIPT IS FOR INTERNAL DEVELOPER USE ONLY!!!
 
-zadig_version=1.1.1.143
+zadig_version=1.1.1.144
 target_dir=/e/dailies/libwdi
 
 type -P git &>/dev/null || { echo "Git not found. Aborting." >&2; exit 1; }
