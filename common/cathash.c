80611ea83787dda0e23dff83c98b37e7e3f43c88
Pete Batard <pbatard@akeo.ie>
2011-11-09 22:24:26 +0000

    [7z-testing] updated cathash.c

--- common/cathash.c
+++ common/cathash.c
@@ -1,9 +1,10 @@
 /*
- *  cathash.c: CAT compliant SHA-1 implementation, for MS PE and data files
+ * cathash v1.0: MS cat file compliant SHA-1 implementation, for PE and data files
+ * Copyright (c) 2011 Pete Batard <pete@akeo.ie>
  *
- *  based on PolarSSL/sha1.c - Copyright (c) 2006-2010, Brainspark B.V. (GPLv2+)
+ * based on PolarSSL/sha1.c - Copyright (c) 2006-2010, Brainspark B.V. (GPLv2+)
  *   http://polarssl.org/trac/browser/trunk/library/sha1.c
- *  based on crypto++/test.cpp by Wei Dai/MaidSafe (Public Domain)
+ * based on crypto++/test.cpp - Copyright Wei Dai/MaidSafe (Public Domain)
  *   http://maidsafe-dht.googlecode.com/svn/trunk/src/maidsafe/cryptopp/test.cpp
  *
  * This program is free software: you can redistribute it and/or modify
@@ -22,6 +23,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <errno.h>
 
@@ -40,11 +42,11 @@ typedef struct
 sha1_context;
 
 /*
- * 32-bit integer manipulation macros (big endian)
+ * 32 & 16 bit integer manipulation macros
  */
 #ifndef GET_ULONG_BE
-#define GET_ULONG_BE(n,b,i)                             \
-{                                                       \
+#define GET_ULONG_BE(n,b,i)                        \
+{                                                  \
     (n) = ( (uint32_t) (b)[(i)    ] << 24 )        \
         | ( (uint32_t) (b)[(i) + 1] << 16 )        \
         | ( (uint32_t) (b)[(i) + 2] <<  8 )        \
@@ -53,12 +55,30 @@ sha1_context;
 #endif
 
 #ifndef PUT_ULONG_BE
-#define PUT_ULONG_BE(n,b,i)                             \
-{                                                       \
-    (b)[(i)    ] = (uint8_t) ( (n) >> 24 );       \
-    (b)[(i) + 1] = (uint8_t) ( (n) >> 16 );       \
-    (b)[(i) + 2] = (uint8_t) ( (n) >>  8 );       \
-    (b)[(i) + 3] = (uint8_t) ( (n)       );       \
+#define PUT_ULONG_BE(n,b,i)                        \
+{                                                  \
+    (b)[(i)    ] = (uint8_t) ( (n) >> 24 );        \
+    (b)[(i) + 1] = (uint8_t) ( (n) >> 16 );        \
+    (b)[(i) + 2] = (uint8_t) ( (n) >>  8 );        \
+    (b)[(i) + 3] = (uint8_t) ( (n)       );        \
+}
+#endif
+
+#ifndef GET_ULONG_LE
+#define GET_ULONG_LE(n,b,i)                        \
+{                                                  \
+    (n) = ( (uint32_t) (b)[(i)    ]       )        \
+        | ( (uint32_t) (b)[(i) + 1] <<  8 )        \
+        | ( (uint32_t) (b)[(i) + 2] << 16 )        \
+        | ( (uint32_t) (b)[(i) + 3] << 24 );       \
+}
+#endif
+
+#ifndef GET_USHORT_LE
+#define GET_USHORT_LE(n,b,i)                       \
+{                                                  \
+    (n) = ( (uint16_t) (b)[(i)    ]       )        \
+        | ( (uint32_t) (b)[(i) + 1] <<  8 );       \
 }
 #endif
 
@@ -328,69 +348,262 @@ void sha1( const uint8_t *input, size_t ilen, uint8_t output[20] )
     memset( &ctx, 0, sizeof( sha1_context ) );
 }
 
+typedef struct {
+    size_t offset;
+    size_t size;
+} range_t;
+
+static void __inline copy_range( range_t* dst, range_t* src )
+{
+    dst->offset = src->offset;
+    dst->size = src->size;
+}
+
+#define SORT_ITEM        range_t
+#define SORT_COPY        copy_range
+#define SORT_CMP( a, b ) ( a.offset < b.offset )
+static void sort(SORT_ITEM a[], size_t n)
+{
+    size_t m, i, j, k;
+    static SORT_ITEM* b = NULL;
+    int need_free = 0;
+
+    if( n < 2 )
+        return;
+
+    if ( b == NULL)
+    {
+        b = (SORT_ITEM*) malloc( sizeof( SORT_ITEM ) * ( n/2 + 1 ) );
+        need_free = 1;
+    }
+
+    m = n >> 1;
+
+    sort( a, m );
+    sort( &a[m], n - m ); 
+
+    for( i=0; i < m; i++ )
+        SORT_COPY( &b[i], &a[i] );
+
+    i = 0; j = m; k = 0;
+    while( ( i < m ) && ( j < n ) )
+        SORT_COPY( &a[k++], ( SORT_CMP( a[j], b[i] ) ) ? &a[j++] : &b[i++] );
+    while( i < m )
+        SORT_COPY( &a[k++], &b[i++] );
+
+    if( need_free )
+        free( b );
+}
+
+/* requires the range table to be sorted by offset with no overlapping or contiguous ranges */
+static size_t skipranges( FILE* f, uint8_t buf[], size_t n, range_t range[], size_t nranges )
+{
+    size_t i, p, wtop, wpos, toread;
+    size_t r = n;
+
+    for( i = 0; i < nranges; i++ )
+    {
+        wtop = ftell(f);
+        wpos = wtop - r;
+        if( ( range[i].offset < wpos ) || ( range[i].offset > wtop ) )
+            continue;
+
+        p = range[i].offset - wpos;
+        if( p + range[i].size > r )
+        {
+            fseek( f, range[i].offset + range[i].size, SEEK_SET );
+            toread = r - p;
+        }
+        else
+        {
+            toread = range[i].size;
+            memmove( &buf[p], &buf[p + toread], r - p - toread );
+        }
+        r += fread( &buf[r - toread], 1, toread, f ) - toread;
+    }
+
+    return r;
+}
+
+/* If running on Windows, we can validate our computation against the native one */
+#define VALIDATE_HASH
+#if defined( VALIDATE_HASH ) && defined( _WIN32 )
+#include <windows.h>
+
+static __inline LPWSTR UTF8toWCHAR( LPCSTR szStr )
+{
+    int size = 0;
+    LPWSTR wszStr = NULL;
+
+    size = MultiByteToWideChar( CP_UTF8, 0, szStr, -1, NULL, 0 );
+    if ( size <= 1 )
+        return NULL;
+
+    if ( ( wszStr = (wchar_t*)calloc( size, sizeof( wchar_t ) ) ) == NULL )
+        return NULL;
+    if ( MultiByteToWideChar( CP_UTF8, 0, szStr, -1, wszStr, size ) != size ) {
+        free( wszStr );
+        return NULL;
+    }
+    return wszStr;
+}
+
+typedef BOOL ( WINAPI *CryptCATAdminCalcHashFromFileHandle_t )(
+    HANDLE hFile,
+    DWORD *pcbHash,
+    BYTE *pbHash,
+    DWORD dwFlags
+);
+
+static BOOL ValidateHash( BYTE* myHash, LPCSTR szfilePath )
+{
+    CryptCATAdminCalcHashFromFileHandle_t CryptCATAdminCalcHashFromFileHandle = NULL;
+    int i;
+    BOOL r = FALSE;
+    HANDLE hFile = NULL;
+    HMODULE h = NULL;
+    BYTE Hash[20];
+    DWORD cbHash = 20;
+    LPWSTR wszFilePath = NULL;
+
+    if ( ( h = GetModuleHandleA( "wintrust" ) ) == NULL )
+        h = LoadLibraryA( "wintrust" );
+    CryptCATAdminCalcHashFromFileHandle =
+        (CryptCATAdminCalcHashFromFileHandle_t) GetProcAddress( h, "CryptCATAdminCalcHashFromFileHandle" );
+    if (CryptCATAdminCalcHashFromFileHandle == NULL)
+       goto out;
+
+    wszFilePath = UTF8toWCHAR( szfilePath );
+    hFile = CreateFileW( wszFilePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL );
+    if ( hFile == INVALID_HANDLE_VALUE )
+        goto out;
+    if ( ( !CryptCATAdminCalcHashFromFileHandle( hFile, &cbHash, Hash, 0 ) ) )
+        goto out;
+    for ( i=0; i < 20; i++ )
+    if( memcmp(Hash, myHash, 20 ) == 0 )
+        r = TRUE;
+    else
+    {
+        printf( "\nThe CAT hash computed by this program differs from the Microsoft one!\n" );
+        printf( "expected: ");
+        for ( i=0; i < 20; i++ )
+            printf( "%02x", Hash[i] );
+        printf( "\n" );
+        printf( "computed: ");
+        for ( i=0; i < 20; i++ )
+            printf( "%02x", myHash[i] );
+        printf( "\n\n" );
+        printf( "please e-mail pete@akeo.ie with your data, so that we address the issue.\n" );
+    }
+
+out:
+    if  (wszFilePath != NULL )
+        free( wszFilePath );
+    if ( hFile )
+        CloseHandle( hFile );
+    return r;
+}
+#endif
+
 /*
- * output = SHA-1( file contents )
+ * output = CAT SHA-1( file contents )
  */
-int main(int argc, char** argv)
+int
+#ifdef DDKBUILD
+__cdecl
+#endif
+main ( int argc, char** argv )
 {
     FILE *f;
-    size_t i, n;
+    size_t i, n, nb_ranges = 0;
     sha1_context ctx;
-    uint8_t buf[0x10400];	/* 65 KB to ensures we can navigate any PE header */
+    uint8_t buf[0x400];
     uint8_t output[20];
-    int process_header = 1;
-    uint16_t optionalHeaderMagic;
-    uint32_t coffPos, optionalHeaderPos, checksumPos, certificateTableDirectoryPos;
+    uint16_t optional_header_magic;
+    uint32_t coff_pos, optional_header_pos, checksum_pos;
+    uint32_t cert_table_directory, cert_table_pos, cert_table_size;
+    range_t range[3];
 
     if( argc < 2 )
     {
-        fprintf( stderr, "usage: sha1cat filename\n");
+        fprintf( stderr, "\ncathash v1.0 - MS compatible SHA-1 for PE and data\n");
+        fprintf( stderr, "Copyright (c) 2011 Pete Batard <pete@akeo.ie>\n\n");
+        fprintf( stderr, "This program is free software: you can redistribute it and/or modify\n");
+        fprintf( stderr, "it under the terms of the GNU General Public License as published by\n");
+        fprintf( stderr, "the Free Software Foundation, either version 3 of the License, or\n");
+        fprintf( stderr, "(at your option) any later version.\n\n");
+        fprintf( stderr, "Usage: cathash filename\n");
         return( 1 );
     }
 
     if( ( f = fopen( argv[1], "rb" ) ) == NULL )
     {
-        fprintf (stderr, "can't open `%s': %s\n", argv[1], strerror( errno ) );
+        fprintf (stderr, "can't open '%s': %s\n", argv[1], strerror( errno ) );
         return( 1 );
     }
-    
+
+    /* PE images have some data sections skipped from hash generation */
+    if( fread( buf, 1, 0x40, f ) == 0x40 )
+    {
+        if( ( buf[0] != 'M' ) || ( buf[1] != 'Z' ) )
+            goto sha1;
+
+        GET_USHORT_LE( coff_pos, buf, 0x3c );
+        optional_header_pos = coff_pos + 0x18;
+        fseek( f, optional_header_pos, SEEK_SET );
+
+        if( fread( buf, 1, 0xa0, f ) != 0xa0 )
+            goto sha1;
+
+        GET_USHORT_LE( optional_header_magic, buf, 0 );
+        if( ( optional_header_magic != 0x10b ) && ( optional_header_magic != 0x20b ) )
+            goto sha1;
+
+        checksum_pos = optional_header_pos + 0x40;
+
+        cert_table_directory = optional_header_magic == 0x10b ? 0x80 : 0x90;
+        GET_ULONG_LE( cert_table_pos, buf, cert_table_directory );
+        GET_ULONG_LE( cert_table_size, buf, cert_table_directory + 4 );
+
+        range[nb_ranges].offset = optional_header_pos + cert_table_directory;
+        range[nb_ranges++].size = 8;
+        range[nb_ranges].offset = checksum_pos;
+        range[nb_ranges++].size = 4;
+        range[nb_ranges].offset = cert_table_pos;
+        range[nb_ranges++].size = cert_table_size;
+
+        sort(range, nb_ranges);
+    }
+
+sha1:
+    fseek(f, 0, SEEK_SET);
     sha1_starts( &ctx );
 
     while( ( n = fread( buf, 1, sizeof( buf ), f ) ) > 0 )
     {
-        if( ( process_header ) && ( buf[0] == 'M' ) && ( buf[1] == 'Z' ) )
-        {
-            process_header = 0;
-            coffPos = *(uint16_t*)&buf[0x3c];														// TODO: handle BE
-            optionalHeaderPos = coffPos + 0x18;
-            optionalHeaderMagic = *(uint16_t*)&buf[optionalHeaderPos];	// TODO: handle BE
-            if( ( optionalHeaderMagic != 0x10b ) && ( optionalHeaderMagic != 0x20b ) )
-                break;
-            printf( "PE32 or PE32+ image identified - using nonstandard (CAT) SHA-1.\n" );
-            checksumPos = optionalHeaderPos + 0x40;
-            /* For CAT SHA-1, we need to ignore the checksum as well as the certificate table pos and size */
-            certificateTableDirectoryPos = optionalHeaderPos + ( optionalHeaderMagic == 0x10b ? 0x80 : 0x90 );
-            n -= 8;
-            memmove(&buf[certificateTableDirectoryPos], &buf[certificateTableDirectoryPos+8], n-certificateTableDirectoryPos);
-            n -= 4;
-            memmove(&buf[checksumPos], &buf[checksumPos+4], n-checksumPos);
-            n += fread( buf+n, 1, 12, f );
-        }
-        sha1_update( &ctx, buf, n );
+        n = skipranges( f, buf, n, range, nb_ranges);
+        if ( n > 0 )
+            sha1_update( &ctx, buf, n );
     }
 
     if( ferror( f ) != 0 )
     {
         fclose( f );
-        fprintf( stderr, "error reading `%s': %s\n", argv[1], strerror( errno ) );
+        fprintf( stderr, "error reading '%s': %s\n", argv[1], strerror( errno ) );
         return( 1 );
     }
 
     fclose( f );
     sha1_finish( &ctx, output );
+
+#if defined( VALIDATE_HASH ) && defined( _WIN32 )
+    if( !ValidateHash( output, argv[1] ) )
+        return( 1 );
+#endif
+
     for ( i=0; i < 20; i++ )
         printf( "%02x", output[i] );
-    printf( "  %s\n", argv[1] );
+    printf( "\n" );
 
     return( 0 );
 }
