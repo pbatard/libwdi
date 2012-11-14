a5d158092396eeda72709e4474ae081ced6d7c66
Pete Batard <pete@akeo.ie>
2012-11-14 02:49:59 +0000

    [internal] produce 2 versions of Zadig (XP/KMDF 1.09 and Vista+/KMDF 1.11)
    * KMDF v1.11, delivered with the WinUSB redist from WDK 8 is not compatible with XP
      => need to produce 2 versions
    * Also update signing process for new certificate
    * Closes #7

--- _bm.sh
+++ _bm.sh
@@ -8,26 +8,34 @@ target_dir=/e/dailies/libwdi
 type -P git &>/dev/null || { echo "Git not found. Aborting." >&2; exit 1; }
 type -P 7zr &>/dev/null || { echo "7-zip (7zr) executable not found. Aborting." >&2; exit 1; }
 
+# Build Zadig for Vista and later (KMDF v1.11)
 git clean -fdx
-(glibtoolize --version) < /dev/null > /dev/null 2>&1 && LIBTOOLIZE=glibtoolize || LIBTOOLIZE=libtoolize
-$LIBTOOLIZE --copy --force || exit 1
-aclocal || exit 1
-autoheader || exit 1
-autoconf || exit 1
-automake -a -c || exit 1
-./configure --disable-shared --enable-toggable-debug --enable-examples-build --disable-debug --with-ddkdir="C:/Program Files (x86)/Windows Kits/8.0" --with-libusb0="D:/libusb-win32" --with-libusbk="D:/libusbK/bin"
+./autogen.sh --disable-shared --with-wdfver=1011
 
 cd libwdi
 make -j2
 cd ../examples
-make zadig.exe
+make -j2 zadig.exe
 # For the app icon to show during UAC, the app needs to have SYSTEM access which MinGW may not grant by default
 # (NB this only matters for local apps - an app extracted from a 7z will always have SYSTEM access)
 # SetACL can be downloaded from http://helgeklein.com/
 type -P SetACL &>/dev/null && { SetACL -on ./zadig.exe -ot file -actn ace -ace "n:S-1-5-18;p:read,read_ex;s:y"; }
-# Can't use WDK's Signtool directly from msys - must use cmd
-cmd.exe /c zadig_sign.bat
-7zr a $target_dir/zadig_v$zadig_version.7z zadig.exe
+cp zadig.exe $target_dir/zadig.exe
 cd ..
 
-scp {$target_dir/zadig_v$zadig_version.7z,examples/zadig_README.creole} pbatard,libwdi@frs.sf.net:/home/pfs/project/l/li/libwdi/zadig
+# Build Zadig for XP (KMDF v1.09)
+git clean -fdx
+./autogen.sh --disable-shared --with-wdfver=1009
+
+cd libwdi
+make -j2
+cd ../examples
+make -j2 zadig.exe
+type -P SetACL &>/dev/null && { SetACL -on ./zadig.exe -ot file -actn ace -ace "n:S-1-5-18;p:read,read_ex;s:y"; }
+cp zadig.exe $target_dir/zadig_xp.exe
+cmd.exe /k zadig_sign.bat "$target_dir/zadig.exe" "$target_dir/zadig_xp.exe"
+7zr a $target_dir/zadig_v$zadig_version.7z $target_dir/zadig.exe
+7zr a $target_dir/zadig_xp_v$zadig_version.7z $target_dir/zadig_xp.exe
+cd ..
+
+#scp {$target_dir/zadig_v$zadig_version.7z,examples/zadig_README.creole} pbatard,libwdi@frs.sf.net:/home/pfs/project/l/li/libwdi/zadig
--- examples/zadig_sign.bat
+++ examples/zadig_sign.bat
@@ -1,6 +1,6 @@
 :retry
-@set password=
 @set /p password=Please enter PFX password: 
-@E:\WinDDK\7600.16385.0\bin\amd64\signtool sign /v /f D:\Secured\akeo\pbatard.p12 /p %password% /t http://time.certum.pl zadig.exe
+@"C:\Program Files (x86)\Windows Kits\8.0\bin\x64\signtool" sign /v /f D:\Secured\akeo\akeo.p12 /p %password% /tr http://timestamp.comodoca.com/rfc3161 %1 %2
 @if ERRORLEVEL 1 goto retry
 @set password=
+@exit
