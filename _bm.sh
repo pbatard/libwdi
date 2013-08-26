d3bc52ffb4f11060c92e9b030119ba15acb8d5da
Pete Batard <pete@akeo.ie>
2013-08-26 02:48:59 +0100

    [misc] update scripts, compile as C, bump version

--- _bm.sh
+++ _bm.sh
@@ -2,11 +2,11 @@
 # Create and upload a Zadig release
 # !!!THIS SCRIPT IS FOR INTERNAL DEVELOPER USE ONLY!!!
 
-zadig_version=2.0.1.160
+zadig_version=2.0.1.161
 target_dir=/e/dailies/libwdi
 
 type -P git &>/dev/null || { echo "Git not found. Aborting." >&2; exit 1; }
-type -P 7zr &>/dev/null || { echo "7-zip (7zr) executable not found. Aborting." >&2; exit 1; }
+type -P upx &>/dev/null || { echo "UPX executable not found. Aborting." >&2; exit 1; }
 
 # Build Zadig for Vista and later (KMDF v1.11)
 git clean -fdx
@@ -20,6 +20,7 @@ make -j2 zadig.exe
 # (NB this only matters for local apps - an app extracted from a 7z will always have SYSTEM access)
 # SetACL can be downloaded from http://helgeklein.com/
 type -P SetACL &>/dev/null && { SetACL -on ./zadig.exe -ot file -actn ace -ace "n:S-1-5-18;p:read,read_ex;s:y"; }
+upx --lzma zadig.exe
 cp zadig.exe $target_dir/zadig.exe
 cd ..
 
@@ -32,10 +33,7 @@ make -j2
 cd ../examples
 make -j2 zadig.exe
 type -P SetACL &>/dev/null && { SetACL -on ./zadig.exe -ot file -actn ace -ace "n:S-1-5-18;p:read,read_ex;s:y"; }
+upx --lzma zadig.exe
 cp zadig.exe $target_dir/zadig_xp.exe
 cmd.exe /k zadig_sign.bat "$target_dir/zadig.exe" "$target_dir/zadig_xp.exe"
-7zr a $target_dir/zadig_v$zadig_version.7z $target_dir/zadig.exe
-7zr a $target_dir/zadig_xp_v$zadig_version.7z $target_dir/zadig_xp.exe
 cd ..
-
-#scp {$target_dir/zadig_v$zadig_version.7z,examples/zadig_README.creole} pbatard,libwdi@frs.sf.net:/home/pfs/project/l/li/libwdi/zadig
