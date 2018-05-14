2b0d7a2f9eed1d996f98008d68a165758b7f916d
Pete Batard <pete@akeo.ie>
2018-05-14 15:34:13 +0100

    [internal] add auto detection of 'git commit --amend'
    * Finally!

---
+++ _detect-amend.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# This script detects whether git commit is being executed in amend or regular mode
+#
+
+# Need to figure out if we are running on Windows or *NIX
+if [ "$(uname -o)" = "Msys" ]; then
+  type -P wmic &>/dev/null || { echo "wmic command not found. Aborting." >&2; exit 1; }
+  type -P grep &>/dev/null || { echo "grep command not found. Aborting." >&2; exit 1; }
+  if $(wmic path win32_process get CommandLine | grep git.exe | grep -q -- --amend); then
+    echo AMEND detected
+    touch ./.amend
+  fi
+else
+  if $(ps -fp $PPID | grep -q -- --amend); then
+    echo AMEND detected
+    touch ./.amend
+  fi
+fi
