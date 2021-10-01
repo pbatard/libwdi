84fe638e15acaf00b2d0836b649a8606b8ca4a3a
Spiro Trikaliotis <spiro.trikaliotis@gmx.de>
2021-10-01 16:15:03 +0200

    cat_file_size in libwdi
    Wrong file size is used when reporting an error that the cat file cannot
    be tokenized. Fixes this. Closes #230.

--- libwdi/libwdi.c
+++ libwdi/libwdi.c
@@ -1403,7 +1403,7 @@ int LIBWDI_API wdi_prepare_driver(struct wdi_device_info* device_info, const cha
 		// Tokenize the cat file (for WDF version)
 		if ((cat_file_size = tokenize_internal(cat_template[driver_type],
 			&dst, inf_entities, "#", "#", 0)) <= 0) {
-			wdi_err("Could not tokenize cat file (%d)", inf_file_size);
+			wdi_err("Could not tokenize cat file (%d)", cat_file_size);
 			r = WDI_ERROR_ACCESS;
 			goto out;
 		}
